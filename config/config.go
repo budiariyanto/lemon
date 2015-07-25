9520361d57100a196b53610052c2b38edb4a8d7b
budiariyanto <fx.budi.ariyanto@gmail.com>
2015-07-26 04:18:18 +0700

    modify config code

--- config/config.go
+++ config/config.go
@@ -1,96 +1,57 @@
 package config
 
 import (
-    "errors"
-    "github.com/pelletier/go-toml"
-    "log"
-    "os"
-    "runtime"
-    "strings"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
 )
 
 const operatingSystem = runtime.GOOS
 
 type Config struct {
-    Path string
-    
 }
 
-func checkConfig(fileName string) (string, error) {
-    var configFileName = fileName
-    var defaultPath string = strings.Join([]string{getDefaultPath(), configFileName}, "")
-    var homePath string = strings.Join([]string{getHomePath(), configFileName}, "")
-    var globalPath string = strings.Join([]string{getGlobalPath(), configFileName}, "")
-
-    if defaultPathInfo, _ := os.Stat(defaultPath); defaultPathInfo != nil {
-        return defaultPath, nil
-    } else if homePathInfo, _ := os.Stat(homePath); homePathInfo != nil {
-        return homePath, nil
-    } else if globalPathInfo, _ := os.Stat(globalPath); globalPathInfo != nil {
-        return globalPath, nil
-    } else {
-        return "", errors.New("[FATAL ERROR]: Unable to find configuration file.")
-    }
+var pathMap map[string]string = map[string]string{
+	"linux_default":   fmt.Sprintf("%s%s", "config", "/"),
+	"darwin_default":  fmt.Sprintf("%s%s", "config", "/"),
+	"windows_default": fmt.Sprintf("%s%s", "config", "\\"),
+	"linux_home":      fmt.Sprintf("%s%s", os.Getenv("HOME"), "/"),
+	"darwin_home":     fmt.Sprintf("%s%s", os.Getenv("HOME"), "/"),
+	"windows_home":    fmt.Sprintf("%s%s", os.Getenv("HOMEPATH"), "\\"),
+	"linux_global":    fmt.Sprintf("%s%s", "/etc", "/"),
+	"darwin_global":   fmt.Sprintf("%s%s", "/etc", "/"),
+	"windows_global":  fmt.Sprintf("%s%s", os.Getenv("SYSTEMROOT"), "\\"),
 }
 
+func (config *Config) CheckConfig(configFileName string) (string, error) {
+	var defaultPath string = fmt.Sprintf("%s%s", pathMap[fmt.Sprintf("%s_%s", operatingSystem, "default")], configFileName)
+	var homePath string = fmt.Sprintf("%s%s", pathMap[fmt.Sprintf("%s_%s", operatingSystem, "home")], configFileName)
+	var globalPath string = fmt.Sprintf("%s%s", pathMap[fmt.Sprintf("%s_%s", operatingSystem, "global")], configFileName)
 
-func getHomePath() string {    
-    var path string
-
-    switch operatingSystem {
-    case "linux":
-        path = strings.Join([]string{os.Getenv("HOME"), "/"}, "")
-    case "darwin":
-        path = strings.Join([]string{os.Getenv("HOME"), "/"}, "")
-    case "windows":
-        path = strings.Join([]string{os.Getenv("HOMEPATH"), "\\"}, "")
-    }
-
-    return path
+	if isConfigFound(defaultPath) {
+		return defaultPath, nil
+	} else if isConfigFound(homePath) {
+		return homePath, nil
+	} else if isConfigFound(globalPath) {
+		return globalPath, nil
+	} else {
+		return "", errors.New("[FATAL ERROR]: Unable to find configuration file.")
+	}
 }
 
-func getGlobalPath() string {
-    var path string
-
-    switch operatingSystem {
-    case "linux":
-        path = strings.Join([]string{os.Getenv("/etc"), "/"}, "")
-    case "darwin":
-        path = strings.Join([]string{os.Getenv("/etc"), "/"}, "")
-    case "windows":
-        path = strings.Join([]string{os.Getenv("SYSTEMROOT"), "\\"}, "")
-    }
-
-    return path
+func isConfigFound(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
 }
 
-func getDefaultPath() string {
-    var path string
+func (config *Config) ReadConfig(path string) ([]byte, error) {
+	content, err := ioutil.ReadFile(path)
 
-    switch operatingSystem {
-    case "linux":
-        path = strings.Join([]string{"config", "/"}, "")
-    case "darwin":
-        path = strings.Join([]string{"config", "/"}, "")
-    case "windows":
-        path = strings.Join([]string{"config", "\\"}, "")
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return path
+	return content, nil
 }
-
-func (conf *Config) Load(fileName string) *toml.TomlTree {
-    configFile, err := checkConfig(fileName)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    config, err := toml.LoadFile(configFile)
-    return config
-}
-
-/*func main() {
-    var config Config
-    tomlConfig := config.Load()
-    fmt.Println(tomlConfig.Get("database.name").(string))
-}*/
--- config/config_test.go
+++ config/config_test.go
@@ -1,1 +1,55 @@
 package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var sampleConfig string = `[database]
+name="postgresql"
+host="localhost"
+port=5432
+dbName="db_cheatsheet"
+username="budi"
+password="budibodonk"
+connectionString="postgresql://budi:budibodonk@localhost/db_cheatsheet"
+
+[log]
+logPath="/path/to/logfile.log"
+`
+
+func TestFindConfigFile(t *testing.T) {
+	config := new(Config)
+	defaultFile := "testconfig.toml"
+	homeFile := fmt.Sprintf("%s%s", pathMap[fmt.Sprintf("%s_%s", operatingSystem, "home")], defaultFile)
+
+	ioutil.WriteFile(defaultFile, []byte(sampleConfig), os.ModePerm)
+	ioutil.WriteFile(homeFile, []byte(sampleConfig), os.ModePerm)
+
+	_, err := config.CheckConfig(defaultFile)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	os.Remove(defaultFile)
+	os.Remove(homeFile)
+}
+
+func TestReadConfig(t *testing.T) {
+	config := new(Config)
+	defaultFile := "testconfig.toml"
+
+	ioutil.WriteFile(defaultFile, []byte(sampleConfig), os.ModePerm)
+
+	content, _ := config.ReadConfig(defaultFile)
+
+	if string(content) != sampleConfig {
+		t.Error("Read config file failed")
+	}
+
+	os.Remove(defaultFile)
+
+}
